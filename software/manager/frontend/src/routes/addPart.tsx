9eb3a09f8c1ae195ad76c690f95a854cede3873e
niwla23 <46248939+niwla23@users.noreply.github.com>
2023-08-05 15:13:54 +0200

    create part only, link and store later

--- software/manager/frontend/src/routes/addPart.tsx
+++ software/manager/frontend/src/routes/addPart.tsx
@@ -2,30 +2,13 @@ import PartAddDataForm from "../components/partAddDataForm"
 import { CreatePartData } from "../types"
 import * as api from "../api"
 import { useNavigate } from "react-router-dom"
-import Swal from "sweetalert2"
 
 export default function AddPart() {
   const navigate = useNavigate()
 
-  const connectToBox = async (partId: number, storeAfterwards: boolean) => {
-    await api.bulkLinkParts([partId], storeAfterwards)
-    navigate("/")
-  }
-
   const submitForm = async (form: CreatePartData, image: File | undefined) => {
     try {
-      const resp = await api.createPart(form, image)
-
-      let result = await Swal.fire({
-        title: "Store Box Now?",
-        icon: "question",
-        showConfirmButton: true,
-        showDenyButton: true,
-        confirmButtonText: "Yes",
-        denyButtonText: "No"
-      })
-
-      await connectToBox(resp.id, result.isConfirmed)
+      await api.createPart(form, image)
       navigate("/")
     } catch (e: any) {
       alert(e.response.data.message)
