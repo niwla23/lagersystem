ec62455993f41b2b4d2e845fe0dce66adb705653
niwla23 <46248939+niwla23@users.noreply.github.com>
2024-12-02 15:05:34 +0100

    Revert "do not recycle position data"
    This reverts commit a95a8c75c390d977b75a0cfc4f42114b10675fec.

--- software/manager/helpers/findPosition.go
+++ software/manager/helpers/findPosition.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"context"
+	"errors"
 
 	"github.com/niwla23/lagersystem/manager/database"
 	ent "github.com/niwla23/lagersystem/manager/ent/generated"
@@ -12,19 +13,19 @@ import (
 func FindPosition(boxX *ent.Box) (*ent.Position, error) {
 	ctx := context.Background()
 	// check if a position is already stored for this box
-	// positionX, err := boxX.QueryPosition().Only(ctx)
+	positionX, err := boxX.QueryPosition().Only(ctx)
 
-	// target := &ent.NotFoundError{}
-	// if errors.As(err, &target) {
-	// we have no position stored, find one
-	positionX, err := database.Client.Position.Query().
-		Where(position.HasWarehouseWith(warehouse.ID(1))).
-		Where(position.Not(position.HasStoredBox())).
-		First(ctx)
-		// } else if err != nil {
+	target := &ent.NotFoundError{}
+	if errors.As(err, &target) {
+		// we have no position stored, find one
+		positionX, err = database.Client.Position.Query().
+			Where(position.HasWarehouseWith(warehouse.ID(1))).
+			Where(position.Not(position.HasStoredBox())).
+			First(ctx)
+	} else if err != nil {
 		// shit has gone terribly wrong
-	// return nil, err
-	// }
+		return nil, err
+	}
 
 	return positionX, err
 }
